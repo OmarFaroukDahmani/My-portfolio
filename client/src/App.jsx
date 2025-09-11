307b02c3d25d4e219718b02a4f1448833f6c999f
Omar Farouk Dahmani <dahmani03112005@gmail.com>
2025-09-11 21:52:21 +0100

    Update routing paths in App component for improved navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,18 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Admin from './pages/Admin'
-import Project from './pages/Project'
 import Signin from './pages/Signin'
+import Projectedit from './pages/Projectedit'
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='home' element={<Home/>} />
-          <Route path='admin' element={<Admin/>} />
-          <Route path='signin' element={<Signin/>} />
-          <Route path='project' element={<Project/>} />
+          <Route path='/' element={<Home/>} />
+          <Route path='/admin' element={<Admin/>} />
+          <Route path='/signin' element={<Signin/>} />
+          <Route path='/admin/edit/:id' element={<Projectedit/>} />
         </Routes>
       </BrowserRouter>
     </>
